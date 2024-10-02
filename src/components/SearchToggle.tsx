0b27e275c1c00bc3b66db3e12de9f7aae247ab0f
yuzuki <yk7hn31@gmail.com>
2024-10-02 18:04:38 +0900

    feat: add search type state and toggle functionality

--- src/components/SearchToggle.tsx
+++ src/components/SearchToggle.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToggleGroup, ToggleGroupItem } from './ui/toggle-group';
 
+type SearchType = 'song' | 'album' | 'artist';
+
 const SearchToggle: React.FC = () => {
+  const [selected, setSelected] = useState<SearchType>('song');
+
+  const handleValueChange = (value: SearchType) => value && setSelected(value);
+
   return (
-    <ToggleGroup type="single" className='justify-start'>
+    <ToggleGroup type="single" className='justify-start'
+      value={selected}
+      onValueChange={handleValueChange}
+    >
       <ToggleGroupItem value="song">Song</ToggleGroupItem>
       <ToggleGroupItem value="album">Album</ToggleGroupItem>
       <ToggleGroupItem value="artist">Artist</ToggleGroupItem>
